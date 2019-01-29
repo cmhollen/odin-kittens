61f0ff1d1f7028c332fbeb0a319644c57f60cb7f
Courtney Hollenkamp <cmhollenkamp@gmail.com>
2019-01-29 11:07:20 -0500

    basic CRUD complete

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,55 @@
+class KittensController < ApplicationController
+
+    def index
+        @kittens = Kitten.all 
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+        if @kitten.save
+            flash[:sucess] = "Kitten created!"
+            redirect_to @kitten
+        else 
+            flash[:error] = "Something went wrong"
+            render 'new'
+        end
+    end
+
+    def show
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def edit
+        @kitten = Kitten.find(params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find(params[:id])
+        if @kitten.update_attributes(kitten_params)
+            flash[:success] = "Kitten updated"
+            redirect_to @kitten
+        else
+            flash[:error] = "Something went wrong"
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @kitten = Kitten.find(params[:id])
+        @kitten.destroy
+        flash[:sucess] = "Kitten deleted"
+        redirect_to root_path
+    end
+
+
+
+    private
+
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
